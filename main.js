3b54529e3e39e3d26b45bab336b537da15cc5a35
yechan-nfc <77843815+yechan-nfc@users.noreply.github.com>
2021-10-02 23:13:51 +0900

    Add bullet

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ canvas.width = 300;
 canvas.height = window.innerHeight - 100;
 
 //var imgShip = new Image();
-//imgShip.src = './img/ship_s.png';
+//imgShip.src = './img/ship.png';
 
 var Ship = {
     x : 135,
@@ -20,8 +20,26 @@ var Ship = {
 }
 Ship.draw();
 
+//var imgBullet = new Image();
+//imgBullet.src = './img/bullet.png';
+
+class Bullet {
+    constructor(){
+        this.x = Ship.x+((Ship.width-10)/2);
+        this.y = Ship.y;
+        this.width = 10;
+        this.height = 10;
+    }
+    draw(){
+        ctx.fillStyle = 'blue';
+        ctx.fillRect(this.x, this.y, this.width, this.height);
+        //ctx.drawImage(imgBullet, this.x, this.y);
+    }
+}
+
+
 //var imgEnemy = new Image();
-//imgEnemy.src = './img/enemy_s.png';
+//imgEnemy.src = './img/enemy.png';
 
 class Enemy {
     constructor(){
@@ -40,7 +58,7 @@ class Enemy {
 
 var timer = 0;
 var enemyArr = [];
-var jumpingCount = 0;
+var bulletArr = [];
 var animation;
 
 function FrameAction(){
@@ -50,20 +68,41 @@ function FrameAction(){
 
     ctx.clearRect(0,0, canvas.width, canvas.height);
 
-    if(timer % 30 == 0){
+    if(timer % Math.floor(Math.random()*200) == 0){
         var enemy = new Enemy();
         enemyArr.push(enemy);
     }
 
-    enemyArr.forEach((a, i, o)=>{
-        // y 좌표가 0미만이면 제거
+    if(timer % 30 == 0){
+        var bullet = new Bullet();
+        bulletArr.push(bullet);
+    }
+
+    bulletArr.forEach((a, i, o)=>{
         if(a.y < 0){
+            o.splice(i, 1);
+        }
+        a.y-=4;
+
+        enemyArr.forEach((b, j, p)=>{
+            if(checkCrash(a, b)){
+                p.splice(j, 1);
+            }
+        })
+
+        a.draw();
+    })
+
+    enemyArr.forEach((a, i, o)=>{
+        if(a.y > canvas.height){
             o.splice(i, 1);
         }
         a.y+=2;
         a.x+=3*(2-Math.floor(Math.random()*5));
 
-        checkCrash(Ship, a);
+        if(checkCrash(Ship, a)){
+            stopGame();
+        }
 
         a.draw();
     })
@@ -87,16 +126,22 @@ function FrameAction(){
 FrameAction();
 
 
-// 충돌확인
+// Check crash between ship and enemy
 function checkCrash(ship, enemy){
     var diffX = (enemy.x + enemy.width) - ship.x;
     var diffY = (enemy.y + enemy.height) - ship.y;
     
     //console.log(diffX+", "+diffY);
     if(diffX < (enemy.width+ship.width) && diffX > 0 && diffY < (enemy.width+ship.width) && diffY > 0){
-        ctx.clearRect(0,0, canvas.width, canvas.height);
-        cancelAnimationFrame(animation);
+        return true;
+    }else{
+        return false;
     }
+}
+
+function stopGame(){
+    ctx.clearRect(0,0, canvas.width, canvas.height);
+    cancelAnimationFrame(animation);
 }
 
 // Input key from Keyboard
