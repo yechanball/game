3a04c0d8061f2b96ff97a3ebca65df851ac38321
yechan-nfc <77843815+yechan-nfc@users.noreply.github.com>
2021-10-04 21:35:29 +0900

    Add control to use sensor API

--- main.js
+++ main.js
@@ -44,7 +44,6 @@ class Bullet {
 class Enemy {
     constructor(){
         this.x = Math.floor(Math.random()*10)*30;
-        //this.x = 165;
         this.y = 0;
         this.width = 30;
         this.height = 30;
@@ -57,21 +56,40 @@ class Enemy {
 }
 
 var timer = 0;
+var score = 0;
 var enemyArr = [];
+var enemyFreq = 200;
+var enemySpeed = 2;
 var bulletArr = [];
+var bulletSpeed = 4;
+
 var animation;
 
 function FrameAction(){
     animation = requestAnimationFrame(FrameAction);
     timer++;
-    ($("#score")).html("점수: "+parseInt(timer/100));
+    ($("#score")).html("점수: "+(score+parseInt(timer/100)));
 
     ctx.clearRect(0,0, canvas.width, canvas.height);
 
-    if(timer % Math.floor(Math.random()*200) == 0){
+    if(timer % Math.floor(Math.random()*enemyFreq) == 0){
         var enemy = new Enemy();
         enemyArr.push(enemy);
     }
+
+    enemyArr.forEach((a, i, o)=>{
+        if(a.y > canvas.height){
+            o.splice(i, 1);
+        }
+        a.y+=enemySpeed;
+        a.x+=3*(2-Math.floor(Math.random()*5));
+
+        if(checkCrash(Ship, a)){
+            stopGame();
+        }
+
+        a.draw();
+    })
 
     if(timer % 30 == 0){
         var bullet = new Bullet();
@@ -82,42 +100,41 @@ function FrameAction(){
         if(a.y < 0){
             o.splice(i, 1);
         }
-        a.y-=4;
+        a.y-=bulletSpeed;
 
         enemyArr.forEach((b, j, p)=>{
             if(checkCrash(a, b)){
                 p.splice(j, 1);
+                score++;
             }
         })
 
         a.draw();
     })
 
-    enemyArr.forEach((a, i, o)=>{
-        if(a.y > canvas.height){
-            o.splice(i, 1);
-        }
-        a.y+=2;
-        a.x+=3*(2-Math.floor(Math.random()*5));
+    // sensor control
+    sensorControl(Ship, canvas);
 
-        if(checkCrash(Ship, a)){
-            stopGame();
-        }
-
-        a.draw();
-    })
-
+    // key control
     if(leftKey == true){
-        Ship.x-=4;
+        if(Ship.x > 0){
+            Ship.x-=4;
+        }
     }
     if(rightKey == true){
-        Ship.x+=4;
+        if(Ship.x < (canvas.width-Ship.width)){
+            Ship.x+=4;
+        }
     }
     if(upKey == true){
-        Ship.y-=4;
+        if(Ship.y > 0){
+            Ship.y-=4;
+        }
     }
     if(downKey == true){
-        Ship.y+=4;
+        if(Ship.y < (canvas.height-Ship.height)){
+            Ship.y+=4;
+        }
     }
 
     Ship.draw();
@@ -183,3 +200,25 @@ document.onkeyup = function(e){
             break;
     }
 };
+
+// Input sensor from mobile device
+function sensorControl(ship, canvas){
+    let gravitySensor = new GravitySensor({frequency: 60});
+
+    // X 양수 왼쪽 아래로, 음수 오른쪽 아래로
+    if(ship.x > 0){
+        ship.x -= gravitySensor.x;
+    }
+    if(ship.x < (canvas.width-ship.width)){
+        ship.x -= gravitySensor.x;
+    }
+    // Y 양수 아래쪽 아래로, 음수 위쪽 아래로
+    if(ship.y > 0){
+        ship.y -= gravitySensor.y;
+    }
+    if(ship.y < (canvas.height-ship.height)){
+        ship.y += gravitySensor.y;
+    }
+
+    gravitySensor.start();
+}
